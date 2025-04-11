ee0939186453044b218eb9e58652695348422754
yangmengke <yangmengke@meituan.com>
2025-04-11 20:14:46 +0800

    配置环境-3

---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/log-64f242be68ec1725.d
@@ -0,0 +1,10 @@
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/liblog-64f242be68ec1725.rmeta: /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/lib.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/macros.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/serde.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/__private_api.rs
+
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/liblog-64f242be68ec1725.rlib: /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/lib.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/macros.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/serde.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/__private_api.rs
+
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/log-64f242be68ec1725.d: /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/lib.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/macros.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/serde.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/__private_api.rs
+
+/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/lib.rs:
+/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/macros.rs:
+/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/serde.rs:
+/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/__private_api.rs:
---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/os-82f465b2fff02113.d
@@ -0,0 +1,13 @@
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-82f465b2fff02113: src/main.rs src/console.rs src/lang_items.rs src/logging.rs src/sbi.rs src/boards/qemu.rs src/entry.asm
+
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-82f465b2fff02113.d: src/main.rs src/console.rs src/lang_items.rs src/logging.rs src/sbi.rs src/boards/qemu.rs src/entry.asm
+
+src/main.rs:
+src/console.rs:
+src/lang_items.rs:
+src/logging.rs:
+src/sbi.rs:
+src/boards/qemu.rs:
+src/entry.asm:
+
+# env-dep:LOG=DEBUG
